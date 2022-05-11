e6b9a21d895c07ab75272dc2722acb0d9bd1ed38
Vasilis <30999851+bs1gr@users.noreply.github.com>
2022-05-11 22:05:36 +0300

    first commit with basic documenting

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var __dirname = "./";
+/* Modules to be used */
+const express = require('express'); // a minimal and flexible web application framework on top of Node.js web server
+const app = express(); 				// enables interacting with the file system
+const fs = require("fs"); 			// The Node.js file system module allows for work with the computer's file system
+const bodyParser = require("body-parser");  // parses the JSON, buffer, string and URL encoded data submitted
 
-const axios = require('axios');
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// request for users list
 app.get('/listUsers', function (req, res) {
-	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://localhost:27017/";
+	const MongoClient = require('mongodb').MongoClient;
+	const url = "mongodb://127.0.0.1:27017/";
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("library2");
+		const dbo = db.db("wss551");
 		dbo.collection("users").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log(result);
@@ -24,22 +24,21 @@ app.get('/listUsers', function (req, res) {
 })
 
 // application -------------------------------------------
-app.get('/', function(req, res) {
-// load the single view file (angular will handle the page
-// changes on the front-end)
-res.sendFile('index.html', { root: __dirname });
+app.get('/',  (req, res) => {
+// load the single view file (angular will handle the page changes on the front-end)
+	res.sendFile('index.html', { root: __dirname });
 });
 
 
 app.post('/addUser', function (req, res) {
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://127.0.0.1:27017/";
 MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	var dbo = db.db("library2");
+	const dbo = db.db("wss551");
 	// You can accept the same input in the form of JSON using Ajax call
 	// but for demonstration purpose, we are hard-coding it here.
-	var user = { "name" : "mohit", "password" : "password4", "profession" :
+	const user = { "name" : "mohit", "password" : "password4", "profession" :
 	"teacher", "id": 4 }
 	dbo.collection("users").insertOne(user, function(err, data) {
 	if (err) throw err;
@@ -51,8 +50,8 @@ MongoClient.connect(url, function(err, db) {
 
 /* app.get('/get_external_api/users/:id', function (req, res) {
 	console.log("okay");
-	var key = req.params.id;
-	var url = "https://jsonplaceholder.typicode.com/users/" + key;
+	const key = req.params.id;
+	const url = "https://jsonplaceholder.typicode.com/users/" + key;
 
 	console.log("calling axios");
 	axios
@@ -70,14 +69,14 @@ MongoClient.connect(url, function(err, db) {
 }) */
 
 app.get('/getUser/:id', function (req, res) {
-	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://localhost:27017/";
+	const MongoClient = require('mongodb').MongoClient;
+	const url = "mongodb://127.0.0.1:27017/";
 	MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	var dbo = db.db("library2");
-	var key = "id";
-	var value = parseInt(req.params.id);
-	var query = { };
+	const dbo = db.db("wss551");
+	const key = "id";
+	const value = parseInt(req.params.id);
+	const query = { };
 	query[key] = value;
 	dbo.collection("users").find(query).toArray(function(err, data) {
 	if (err) throw err;
@@ -89,12 +88,12 @@ app.get('/getUser/:id', function (req, res) {
 })
 
 app.delete('/deleteUser/:id', function (req, res) {
-	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://localhost:27017/";
+	const MongoClient = require('mongodb').MongoClient;
+	const url = "mongodb://127.0.0.1:27017/";
 	MongoClient.connect(url, function(err, db) {
 	if (err) throw err;
-	var dbo = db.db("library2");
-	var query = { id: 4 }; dbo.collection("users").deleteOne(query,
+	const dbo = db.db("wss551");
+	const query = { id: 4 }; dbo.collection("users").deleteOne(query,
 	function(err, data) {
 	if (err) throw err;
 	console.log("1 document deleted");
@@ -113,9 +112,35 @@ res.end( "DONE!");
 // Gia na valume eikones
 app.use(express.static('assets'))
 
+
+
+/* On Error */
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('File Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.send(err.message);
+});
+
 // Prepei na bei sto telos
-var server = app.listen(8081, function () {
-var host = server.address().address
-var port = server.address().port
-console.log("Example app listening at http://%s:%s", host, port)
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, function () {
+	console.log('Server is started on http://127.0.0.1:'+PORT);
+});
+/*
+
+// Prepei na bei sto telos
+const server = app.listen(8081, function () {
+	const host = server.address().address
+	const port = server.address().port
+	console.log("Example app listening at http://%s:%s", host, port)
 })
+*/
